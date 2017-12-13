b44c291c7e606ab6d42f85212f13bb330bed4e4c
Mateus Rodrigues <devrdgs@gmail.com>
2017-12-13 21:52:07 -0200

    Finished tests for generic search

--- src/index.js
+++ src/index.js
@@ -1,1 +1,10 @@
+/* eslint-env browser */
 
+export const search = (query, type) =>
+  fetch(`https://api.spotify.com/v1/search?q=${query}&type=${type}`)
+    .then(response => response);
+
+export const searchAlbums = () => {};
+export const searchArtists = () => {};
+export const searchTracks = () => {};
+export const searchPlaylists = () => {};
--- tests/index.spec.js
+++ tests/index.spec.js
@@ -1,13 +1,70 @@
 /* eslint-env mocha */
 
-import * as chai from 'chai';
+import chai, { expect } from 'chai';
 import dirtyChai from 'dirty-chai';
+import sinon from 'sinon';
+import sinonChai from 'sinon-chai';
+import sinonStubPromise from 'sinon-stub-promise';
+import { search, searchAlbums, searchArtists, searchTracks, searchPlaylists } from '../src/index';
 
-const { expect } = chai;
+global.fetch = require('node-fetch');
 
 chai.use(dirtyChai);
+chai.use(sinonChai);
+sinonStubPromise(sinon);
 
-describe('Index', () => {
-
+describe('Spotify wrapper', () => {
+  describe('Smoke tests', () => {
+    it('should exists the search method', () => {
+      expect(search).to.exist();
+    });
+    it('should exists the searchAlbums method', () => {
+      expect(searchAlbums).to.exist();
+    });
+    it('should exists the searchArtists method', () => {
+      expect(searchArtists).to.exist();
+    });
+    it('should exists the searchTracks method', () => {
+      expect(searchTracks).to.exist();
+    });
+    it('should exists the searchPlaylists method', () => {
+      expect(searchPlaylists).to.exist();
+    });
+  });
+  describe('Generic search', () => {
+    let fetchedStub;
+    let promise;
+    beforeEach(() => {
+      fetchedStub = sinon.stub(global, 'fetch');
+      promise = fetchedStub.returnsPromise();
+    });
+    afterEach(() => {
+      fetchedStub.restore();
+    });
+    it('should call fetch function', () => {
+      const artists = search();
+      expect(fetchedStub).to.have.been.calledOnce();
+    });
+    it('should receive the correct url to fetch', () => {
+      context('passing one type only', () => {
+        const artists = search('Metallica', 'artist');
+        expect(fetchedStub).to.have.been
+          .calledWith('https://api.spotify.com/v1/search?q=Metallica&type=artist');
+        const albums = search('Metallica', 'album');
+        expect(fetchedStub).to.have.been
+          .calledWith('https://api.spotify.com/v1/search?q=Metallica&type=album');
+      });
+      context('passing more than one type', () => {
+        const artistsAndAlbums = search('Metallica', ['artist', 'album']);
+        expect(fetchedStub).to.have.been
+          .calledWith('https://api.spotify.com/v1/search?q=Metallica&type=artist,album');
+      });
+    });
+    it('should return the JSON Data from the Promise', () => {
+      promise.resolves({ body: 'json' });
+      const artists = search('Metallica', 'artist');
+      expect(artists.resolveValue).to.be.eql({ body: 'json' });
+    });
+  });
 });
 
