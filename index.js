5da7f4b0ded6df29daf40833020dbe9783b46982
Mateus Rodrigues <devrdgs@gmail.com>
2017-12-18 12:00:00 -0200

    updated index.js

--- index.js
+++ index.js
@@ -1,5 +1,14 @@
-import { getAlbum, getAlbums, getAlbumTracks } from './src/index';
+import SpotifyWrapper from './src/index';
 
-getAlbum('1E72ED2DF9qg7Dr08yCkHx').then(response => console.log(response));
-getAlbums(['2k2dsjEAwQVhmjAyMQCCcl', '60B5KOeXnnJVHWqWcXH56f']).then(response => console.log(response));
-getAlbumTracks(['7l0L2YHlQwAyI4QyZTIWGS']).then(response => console.log(response));
+const spotifyWrapper = new SpotifyWrapper({
+  apiURL: 'https://api.spotify.com/v1',
+  token: 'BQB2_lKkq4WoJnMqQqjF4jTKt8ctUrK_q-YSS1Nsl1lZFCuuh4nvxX_8VY2emRjLZTxefDKLJAGO9xl1aEI-ITrCtks_mSoe5BT0rXhQGf6P-oRS0-f7XsE9Jv5VQLid4fkvlHjII8Z1tT6e4z3PUK3h-1STTyMQ_2IKng',
+});
+
+spotifyWrapper
+  .search.searchArtist('DVBBS')
+  .then(response => response.json())
+  .then((data) => {
+    const { items } = data.artists;
+    console.log(items);
+  });
